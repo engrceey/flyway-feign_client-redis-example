b99b8e09045e08049b40a35d83e8d3a80ba80992
zurum <zurumogbonda@gmail.com>
2022-08-22 23:36:43 +0100

    add flyway script customer

---
+++ src/main/java/com/zurum/flywayredisfeignclientexample/migrations/V1/V1_1__CreateCustomerProfile.java
@@ -0,0 +1,20 @@
+package com.zurum.flywayredisfeignclientexample.migrations.V1;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+
+public class V1_1__CreateCustomerProfile extends BaseJavaMigration {
+    @Override
+    public void migrate(Context context) throws Exception {
+        final String sql = "CREATE TABLE IF NOT EXISTS customer_profile ("+
+                " customer_id SERIAL, "+
+                "id_string VARCHAR(255)  NOT NULL, "+
+                "customer_name VARCHAR(50) NOT NULL, "+
+                "customer_balance DECIMAL, "+
+                "creation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "+
+                "date_of_birth TIMESTAMP DEFAULT NULL, "+
+                "PRIMARY KEY (customer_id)"+
+                ");";
+        context.getConnection().createStatement().execute(sql);
+    }
+}
