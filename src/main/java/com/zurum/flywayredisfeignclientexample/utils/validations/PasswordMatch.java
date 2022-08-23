4d45ff0a01ae87718e20e56dfa22f70b08ef7393
zurum <zurumogbonda@gmail.com>
2022-08-23 01:10:28 +0100

    add utils

---
+++ src/main/java/com/zurum/flywayredisfeignclientexample/utils/validations/PasswordMatch.java
@@ -0,0 +1,20 @@
+package com.zurum.flywayredisfeignclientexample.utils.validations;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.TYPE_USE;
+
+@Documented
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy=PasswordMatchValidator.class)
+public @interface PasswordMatch {
+    String message() default "Password and Confirm Password do not match.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
