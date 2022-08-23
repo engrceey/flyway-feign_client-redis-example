f97c3c74aa33b562768ea982945a97a636ebf0d8
zurum <zurumogbonda@gmail.com>
2022-08-24 00:29:18 +0100

    custom serializer

---
+++ src/main/java/com/zurum/flywayredisfeignclientexample/utils/CustomLocalDateSerializer.java
@@ -0,0 +1,21 @@
+package com.zurum.flywayredisfeignclientexample.utils;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class CustomLocalDateSerializer extends StdSerializer<LocalDate> {
+
+    public CustomLocalDateSerializer() {
+        super(LocalDate.class);
+    }
+
+    @Override
+    public void serialize(LocalDate localDate, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeString(localDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+    }
+}
---
+++ src/main/java/com/zurum/flywayredisfeignclientexample/utils/CustomLocalDateTimeSerializer.java
@@ -0,0 +1,19 @@
+package com.zurum.flywayredisfeignclientexample.utils;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class CustomLocalDateTimeSerializer extends StdSerializer<LocalDateTime> {
+    public CustomLocalDateTimeSerializer() {
+        super(LocalDateTime.class);
+    }
+    @Override
+    public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        gen.writeString(value.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    }
+}
