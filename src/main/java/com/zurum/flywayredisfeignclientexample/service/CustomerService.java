61a77e441272e0cd9b261a9dad5b68a6939cba2b
zurum <zurumogbonda@gmail.com>
2022-08-23 01:10:53 +0100

    update service

--- src/main/java/com/zurum/flywayredisfeignclientexample/service/CustomerService.java
+++ src/main/java/com/zurum/flywayredisfeignclientexample/service/CustomerService.java
@@ -1,4 +1,14 @@
 package com.zurum.flywayredisfeignclientexample.service;
 
+import com.zurum.flywayredisfeignclientexample.dto.request.CreateCustomerRequest;
+import com.zurum.flywayredisfeignclientexample.dto.request.UpdateCustomerRequest;
+import com.zurum.flywayredisfeignclientexample.dto.response.GetCustomerResponse;
+
 public interface CustomerService {
+    String registerCustomer(CreateCustomerRequest customerRequest);
+    GetCustomerResponse getCustomerById(String id);
+
+    String updateCustomer(String id, UpdateCustomerRequest updateCustomerRequest);
+
+    String deleteCustomer(String id);
 }
--- src/main/java/com/zurum/flywayredisfeignclientexample/service/implementations/CustomerServiceImpl.java
+++ src/main/java/com/zurum/flywayredisfeignclientexample/service/implementations/CustomerServiceImpl.java
@@ -1,4 +1,56 @@
 package com.zurum.flywayredisfeignclientexample.service.implementations;
 
-public class CustomerServiceImpl {
+import com.zurum.flywayredisfeignclientexample.dto.request.CreateCustomerRequest;
+import com.zurum.flywayredisfeignclientexample.dto.request.UpdateCustomerRequest;
+import com.zurum.flywayredisfeignclientexample.dto.response.GetCustomerResponse;
+import com.zurum.flywayredisfeignclientexample.model.Customer;
+import com.zurum.flywayredisfeignclientexample.repository.CustomerRepository;
+import com.zurum.flywayredisfeignclientexample.service.CustomerService;
+import com.zurum.flywayredisfeignclientexample.utils.ModelMapperUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+    @Override
+    public String registerCustomer(CreateCustomerRequest customerRequest) {
+
+//        if(isCustomerAvailable())
+        return null;
+    }
+
+    @Override
+    public GetCustomerResponse getCustomerById(String id) {
+        return ModelMapperUtils.map(customerRepository.findByIdString(id).orElseThrow(() -> {
+           throw new RuntimeException("Not found");
+        }), GetCustomerResponse.class);
+    }
+
+    @Override
+    public String updateCustomer(String id, UpdateCustomerRequest updateCustomerRequest) {
+        Customer customer = customerRepository.findByIdString(id).orElseThrow(() -> {
+            throw new RuntimeException("Not found");
+        });
+
+        ModelMapperUtils.map(updateCustomerRequest, customer);
+        return "yes";
+    }
+
+    @Override
+    public String deleteCustomer(final String id) {
+        if (isCustomerAvailable(id)) {
+            customerRepository.deleteById(customerRepository.findByIdString(id).get().getCustomerId());
+        }
+        throw new RuntimeException("Not found");
+    }
+
+    private boolean isCustomerAvailable(String id) {
+        return customerRepository.findByIdString(id).isPresent();
+
+    }
 }
